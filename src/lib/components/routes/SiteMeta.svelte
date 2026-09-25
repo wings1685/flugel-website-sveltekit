ae7797b5db0b1fa9103e73ddefecb5fca4a802e8
Yutaka Imagawa <flugel.biz@gmail.com>
2026-09-25 11:27:59 +0900

    Meta 展開コンポーネントの名前を変更

--- src/lib/components/routes/SiteMeta.svelte
+++ src/lib/components/routes/SiteMeta.svelte
@@ -3,7 +3,7 @@
 	import { buildMeta } from "$lib/_global/lib/meta";
 	import type { DeepGuard } from "$lib/_global/types/types";
 	import type { YamlFiles, MetaProps } from "$lib/_global/lib/meta";
-	import { Meta } from "../shared/Utils";
+	import { MetaTags } from "../shared/Utils";
 
 	const globData = import.meta.glob('./**/_data/meta.yaml', { eager: true }) as YamlFiles;
 
@@ -11,4 +11,4 @@
 	const pageProps = untrack(() => props);
 	const meta = buildMeta({ ...pageProps, globData });
 </script>
-<Meta { ...meta } />
+<MetaTags { ...meta } />
--- src/lib/components/shared/Utils/index.ts
+++ src/lib/components/shared/Utils/index.ts
@@ -1,3 +1,3 @@
 export { default as Bg } from "./Bg.svelte";
-export { default as Meta } from "./Meta.svelte";
+export { default as MetaTags } from "./MetaTags.svelte";
 export { default as Icon } from "./Icon.svelte";
