804ca6e7df1334ab0e8326ea331e86fe824ad237
Yutaka Imagawa <flugel.biz@gmail.com>
2026-09-26 00:02:57 +0900

    非使用 export の解除

--- src/lib/_global/lib/meta.ts
+++ src/lib/_global/lib/meta.ts
@@ -6,7 +6,7 @@ const MetaSchema = v.object({
 	description: v.optional(v.string()),
 	ogImage: v.optional(v.string()),
 });
-export type SiteMeta = v.InferOutput<typeof MetaSchema>;
+type SiteMeta = v.InferOutput<typeof MetaSchema>;
 type PageMeta = Partial<SiteMeta>;
 export type MetaData = SiteMeta & {
 	siteTitle: string,
